ed036bebf406df4c083222b2e2303f64ef16fc6b
“CaitlinYoung7” <caitlincnyoung@gmail.com>
2025-02-09 16:13:51 +0000

    Input bar appears from bottom

--- src/InputArea/InputArea.jsx
+++ src/InputArea/InputArea.jsx
@@ -3,8 +3,11 @@ import Form from "./Form/Form.jsx";
 
 function InputArea({ addText }) {
   return (
-    <div className="input-bar">
-      <Form addText={addText} />
+    <div className="bar-container">
+      <span className="arrow">⬆️</span>
+      <div className="input-bar">
+        <Form addText={addText} />
+      </div>
     </div>
   );
 }
